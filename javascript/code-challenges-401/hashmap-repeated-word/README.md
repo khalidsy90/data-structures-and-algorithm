15cddff8f9080a44e0d9188e3f2e825f80ae8866
khalidsy90 <khalidhamedi3@gmail.com>
2021-12-18 21:06:58 +0200

    compelte hashmap-repeated-word

---
+++ javascript/code-challenges-401/hashmap-repeated-word/README.md
@@ -0,0 +1,32 @@
+# Code Challenge: Class 31 / hashmap-repeated-word
+
+## Challenge
+
+### Write a function called repeated word that finds the first word to occur more than once in a string,Arguments string and Return string.
+
+![img](challenge31-whiteboard.png)
+
+## Approach
+
+- ### I understood the problem first
+- ### I imagined how the results should be
+- ### I made a drawings of how the hashTable would be after insertion of multiple key and values.
+- ### I wrote the code
+- ### I made the tests
+
+## Efficiency
+
+- ### time :O(n)
+- ### space : O(n)
+
+## API
+
+- ### Hashtable
+
+  - repeatedWord(string) : This method Finds the most repeated word in a book.
+
+## Testing
+
+### [Tests](https://github.com/Duniaalkilany/data-structures-and-algorithms/tree/main/401-challenges/repeated-word/repeated-word.test.js)
+
+![img](challenge31-test.png)
---
+++ javascript/code-challenges-401/hashmap-repeated-word/repeated-word.js
@@ -0,0 +1,77 @@
+const LinkedList = require("./LinkedList");
+
+class Hashmap {
+  constructor(size) {
+    this.size = size;
+    this.storage = new Array(size);
+  }
+
+  hash(key) {
+    const sumCharCode = key.split("").reduce((acc, char) => {
+      return acc + char.charCodeAt(0);
+    }, 0);
+    const hashKey = (sumCharCode * 19) % this.size;
+    return hashKey;
+  }
+
+  add(key, value) {
+    // get an index
+    const hash = this.hash(key);
+    // console.log("Key", hash);
+    // we need to check if a value already exist in the index
+    if (!this.storage[hash]) {
+      const ll = new LinkedList();
+      ll.insert({ [key]: value });
+      this.storage[hash] = ll; // [222] = {head:{value:{"key","value"},next:null}}
+    } else {
+      this.storage[hash].insert({ [key]: value });
+    }
+  }
+
+  get(key) {
+    const hash = this.hash(key);
+    if (this.storage[hash]) {
+      let currentNode = this.storage[hash].head;
+      while (currentNode) {
+        if (currentNode.value[key]) {
+          return currentNode.value[key];
+        }
+        currentNode = currentNode.next;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  contains(key) {
+    const hash = this.hash(key);
+    if (this.storage[hash]) {
+      let currentNode = this.storage[hash].head;
+      while (currentNode) {
+        if (currentNode.value[key]) {
+          return true;
+        }
+        currentNode = currentNode.next;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  // repeatedWord
+  repeatedWord(string) {
+    const arr = string.split(/[ ,]+/);
+    let key;
+    let hash;
+    for (let i = 0; i < arr.length; i++) {
+      key = arr[i].toLowerCase();
+      this.add(key, "noValue");
+      hash = this.hash(key);
+      if (this.storage[hash].head.next) {
+        return key;
+      }
+    }
+  }
+}
+
+module.exports = Hashmap;
